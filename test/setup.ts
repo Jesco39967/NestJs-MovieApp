4141441bc047713833dde31a12fe8d8263f7212f
Jesco39967 <Jesco39967@users.noreply.github.com>
2025-06-18 01:47:57 +0000

    Add test setup file with basic configuration

---
+++ test/setup.ts
@@ -0,0 +1,12 @@
+// Global test setup file
+// Add any global test configurations or mocks here
+
+// Example: Disable console logs during tests
+beforeEach(() => {
+  jest.spyOn(console, 'log').mockImplementation(() => {});
+  jest.spyOn(console, 'error').mockImplementation(() => {});
+});
+
+afterEach(() => {
+  jest.restoreAllMocks();
+});
