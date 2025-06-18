15284d7bd2bb6b2d23a11452d983d8c71a19fe07
Jesco39967 <Jesco39967@users.noreply.github.com>
2025-06-18 01:53:39 +0000

    Update test setup to use Vitest vi instead of jest

--- test/setup.ts
+++ test/setup.ts
@@ -1,12 +1,14 @@
+import { vi } from 'vitest';
+
 // Global test setup file
 // Add any global test configurations or mocks here
 
 // Example: Disable console logs during tests
 beforeEach(() => {
-  jest.spyOn(console, 'log').mockImplementation(() => {});
-  jest.spyOn(console, 'error').mockImplementation(() => {});
+  vi.spyOn(console, 'log').mockImplementation(() => {});
+  vi.spyOn(console, 'error').mockImplementation(() => {});
 });
 
 afterEach(() => {
-  jest.restoreAllMocks();
+  vi.restoreAllMocks();
 });
